fe1a1d91bbe840df8ab4103893cafc35761a6122
HaloHub <node.halo@gmail.com>
2026-07-01 16:17:30 +0800

    chore: init project

---
+++ src/test/java/com/halohub/frankenstein/FrankensteinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halohub.frankenstein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankensteinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
